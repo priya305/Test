ebf261c43afe8acd06428176f4e3dac579ff674b
priya999 <pujaaaakumari@gmail.com>
2020-10-30 10:20:10 +0000

    User input

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int a,b,c;
+  printf("enter two number");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("sum=%d",c);
   return 0;
 }
